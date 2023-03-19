f119afd26babe45b0f898fecd69eebc98bb78f4a
Catalina323 <116120497+Catalina323@users.noreply.github.com>
2023-03-20 00:11:34 +0200

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,59 +1,96 @@
-//ShellSort
 #include <iostream>
 #include <fstream>
 #include <chrono>
-
+using namespace std;
 using namespace std::chrono;
 
-std::ifstream fin("numere4.txt");
+ifstream fin("numere1.txt");
 
-void shell(int v[], int n)
+
+long maxval(long v[], long n)
 {
-	int i, j;
-	for (i = n / 2;i > 0;i /= 2)
+	int i, maxim;
+	maxim = v[0];
+	for (i = 1;i < n;i++)
 	{
-		for (j = 0;j < (n - i);j++)
+		if (maxim < v[i])
 		{
-			if (v[j] > v[j + i])
-			{
-				int k = j;
-				while (k >= 0 && v[k] > v[k + i])
-				{
-					std::swap(v[k], v[k + i]);
-					k -= i;
-				}
-			}
+			maxim = v[i];
 		}
 	}
+	return maxim;
+}
+
+void countsort(long v[], long placement, long n)
+{
+	long i, fv[11];
+	int* temp = new int[1000001];
+	for (i = 0;i < 10;i++)
+	{
+		fv[i] = 0;
+	}
+	for (i = 0;i < n;i++)
+	{
+		fv[(v[i] / placement) % 10]++;
+	}
+
+	for (i = 1;i < 10;i++)
+	{
+		fv[i] += fv[i - 1];
+	}
+
+	for (i = n-1;i >= 0;i--)
+	{
+		temp[fv[(v[i] / placement) % 10] -1] = v[i];
+		fv[(v[i] / placement) % 10]--;
+	}
+	for (i= 0;i < n;i++)
+	{
+		v[i] = temp[i];
+	}
+	delete[] temp;
+}
+
+void radixSort(long v[], long n)
+{
+	long placement, maxim;
+	maxim = maxval(v, n);
+	for (placement = 1; maxim / placement > 0;placement *= 10)
+	{
+		countsort(v, placement, n);
+	}
+
 }
 
 int main()
 {
-	int* v = new int[1000001];
-	int n = 0, x;
+	long x, v[1000001], n = 0;
 
 	while (fin >> x)
 	{
 		v[n] = x;
 		n++;
 	}
-	std::cout << n << std::endl;
+
+	//int n = sizeof(v) / sizeof(v[0]);
+
+	cout << n << endl;
 
 	auto start = high_resolution_clock::now();
-	shell(v, n);
+
+	radixSort(v, n);
+
+	cout << "Am terminat de sortat :) " << endl;
+
 	auto stop = high_resolution_clock::now();
 	auto duration = duration_cast<microseconds>(stop - start);
-	std::cout << duration.count()<<std::endl;
-
-	delete[] v;
-
-	std::cout << "Am terminat de sortat :) " << std::endl;
+	cout << duration.count();
 
 	/*
-	for (i = 0;i < n;i++)
-		std::cout << v[i] << " ";
-
-	delete[] v;
-	*/	
+	for (int i = 0;i < n;i++)
+	{
+		std::cout << v[i]<<" ";
+	}
+	*/
 	return 0;
 }
