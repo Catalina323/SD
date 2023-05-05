e1322c020f681429471d2b6b52814155ff7ef79b
Catalina323 <116120497+Catalina323@users.noreply.github.com>
2023-05-05 03:23:51 +0300

    Update Source.cpp

--- Tema 2-AVL tree/Source.cpp
+++ Tema 2-AVL tree/Source.cpp
@@ -16,6 +16,16 @@ class AVL {
 	nod* radacina;
 
 public:
+
+	int calch(nod *n)
+	{
+		if (n == NULL)
+			return 0;
+		else {
+			return n->inaltime;
+		}
+	}
+
 	nod* Insert(nod* node, int valoare)
 	{
 		if (node == NULL)
@@ -34,21 +44,58 @@ public:
 			}
 		}
 		
+		//facem inaltimea nodului node
+		if (calch(node->dreapta) > calch(node->stanga))
+		{
+			node->inaltime = 1 + calch(node->dreapta);
+		}
+		else {
+			node->inaltime = 1 + calch(node->stanga);
+		}
 
+		int balance;
+		if (node == NULL)
+			balance = 0;
+		else 
+			balance = node->stanga->inaltime - node->dreapta->inaltime;
+
+		/*
+		// Left Left Case
+		if (balance > 1 && valoare < node->stanga->valoare)
+			return rightRotate(node);
+
+		// Right Right Case
+		if (balance < -1 && valoare > node->dreapta->valoare)
+			return leftRotate(node);
+
+		// Left Right Case
+		if (balance > 1 && valoare > node->stanga->key)
+		{
+			node->stanga = leftRotate(node->stanga);
+			return rightRotate(node);
+		}
+
+		// Right Left Case
+		if (balance < -1 && valoare < node->dreapta->valoare)
+		{
+			node->dreapta = rightRotate(node->dreapta);
+			return leftRotate(node);
+		}
+
+		*/
+
+		//return the (unchanged) node pointer 
 		return node;
 	}
-
-
 
 };
 
 int main()
 {
 	AVL copacel;
-	nod* radacina = NULL;
-	radacina = copacel.Insert(radacina, 10);
-	radacina = copacel.Insert(radacina, 20);
-	radacina = copacel.Insert(radacina, 5);
+	nod* radacina = copacel.Insert(NULL, 10);
+	copacel.Insert(radacina, 20);
+	copacel.Insert(radacina, 5);
 
 
 	return 0;
