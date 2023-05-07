fe2c69ad4e5f6ed23bdaa0b09561f083e041d063
Catalina323 <116120497+Catalina323@users.noreply.github.com>
2023-05-07 21:10:16 +0300

    Update Source.cpp

--- Tema 2-AVL tree/Source.cpp
+++ Tema 2-AVL tree/Source.cpp
@@ -1,6 +1,9 @@
 //AVL tree
 #include<iostream>
+#include <fstream>
 
+std::ifstream fin("abce.in");
+std::ofstream fout("abce.out");
 
 struct nod {
 
@@ -10,10 +13,10 @@ struct nod {
 	int inaltime;
 
 	nod(int valoare) : valoare(valoare), stanga(NULL), dreapta(NULL), inaltime(1) {}
+
 };
 
 class AVL {
-	nod* radacina;
 
 public:
 
@@ -25,7 +28,38 @@ public:
 			return n->inaltime;
 		}
 	}
+	nod* succ_jos(nod* n)
+	{
+	
+		nod* dr = n->dreapta;
+		if (dr != NULL)
+		{
+			while (dr->stanga != NULL)
+				dr = dr->stanga;
 
+			return dr;
+		}
+		else
+			return n;
+	}
+	int balance(nod* n)
+	{
+		int balance;
+		if (n == NULL || (n->stanga == NULL && n->dreapta == NULL))
+			balance = 0;
+		else
+		{
+			if (n->stanga == NULL)
+				balance = 0 - n->dreapta->inaltime;
+			else if (n->dreapta == NULL)
+				balance = n->stanga->inaltime;
+			else
+				balance = n->stanga->inaltime - n->dreapta->inaltime;
+		}
+
+		return balance;
+
+	}
 	nod* R_Stanga(nod* n)
 	{
 		nod* dr = n->dreapta;
@@ -52,9 +86,9 @@ public:
 			dr->inaltime = 1 + calch(dr->stanga);
 		}
 
-		return n;
+		//return n;
+		return dr;
 	}
-
 	nod* R_Dreapta(nod* n)
 	{
 		nod* st = n->stanga;
@@ -81,11 +115,9 @@ public:
 			st->inaltime = 1 + calch(st->stanga);
 		}
 
-		return n;
+		//return n;
+		return st;
 	}
-
-
-
 	nod* Insert(nod* node, int valoare)
 	{
 		if (node == NULL)
@@ -113,37 +145,168 @@ public:
 			node->inaltime = 1 + calch(node->stanga);
 		}
 
-		int balance;
-		if (node == NULL)
-			balance = 0;
-		else 
-			balance = node->stanga->inaltime - node->dreapta->inaltime;
-
-		
-		// Left Left Case
-		if (balance > 1 && valoare < node->stanga->valoare)
+		// Cazul stanga-stanga
+		if (balance(node) > 1 && balance(node->stanga) >= 0) {
 			return R_Dreapta(node);
+		}
 
-		// Right Right Case
-		if (balance < -1 && valoare > node->dreapta->valoare)
-			return R_Stanga(node);
-
-		// Left Right Case
-		if (balance > 1 && valoare > node->stanga->valoare)
-		{
+		// Cazul stanga-dreapta
+		if (balance(node) > 1 && balance(node->stanga) < 0) {
 			node->stanga = R_Stanga(node->stanga);
 			return R_Dreapta(node);
 		}
 
-		// Right Left Case
-		if (balance < -1 && valoare < node->dreapta->valoare)
-		{
+		// Cazul dreapta-dreapta
+		if (balance(node) < -1 && balance(node->dreapta) <= 0) {
+			return R_Stanga(node);
+		}
+
+		// Cazul dreapta-stanga
+		if (balance(node) < -1 && balance(node->dreapta) > 0) {
 			node->dreapta = R_Dreapta(node->dreapta);
 			return R_Stanga(node);
 		}
 
-		//return the (unchanged) node pointer 
 		return node;
+	}
+	nod* Delete(nod* node, int valoare)
+	{
+		if (node == NULL)
+			return node;
+		else if (valoare < node->valoare)
+			node->stanga = Delete(node->stanga, valoare);
+		else if (valoare > node->valoare)
+			node->dreapta = Delete(node->stanga, valoare);
+		else
+		{
+			nod* st = node->stanga;
+			nod* dr = node->dreapta;
+			nod* temp = NULL;
+			
+			if (st == NULL && dr == NULL)
+			{
+				//nodul nu are copii
+				node = temp;
+			}
+			else if (st == NULL || dr == NULL)
+			{
+				//nodul are un copil
+				if (st != NULL)
+				{
+					node = st;
+					st = temp;
+				}
+				else if(dr != NULL){
+					node = dr;
+					dr = temp;
+				}
+			}
+			else {
+				//nodul are 2 copii
+				nod* succ = succ_jos(node);
+				node->valoare = succ->valoare;
+				node->dreapta = Delete(node->dreapta, succ->valoare);
+			}
+		}
+
+		//refacem structura AVL
+		//inaltimea
+		if (calch(node->dreapta) > calch(node->stanga))
+		{
+			node->inaltime = 1 + calch(node->dreapta);
+		}
+		else {
+			node->inaltime = 1 + calch(node->stanga);
+		}
+
+		//balance
+
+		// Cazul stanga-stanga
+		if (balance(node) > 1 && balance(node->stanga) >= 0) {
+			return R_Dreapta(node);
+		}
+
+		// Cazul stanga-dreapta
+		if (balance(node) > 1 && balance(node->stanga) < 0) {
+			node->stanga = R_Stanga(node->stanga);
+			return R_Dreapta(node);
+		}
+
+		// Cazul dreapta-dreapta
+		if (balance(node) < -1 && balance(node->dreapta) <= 0) {
+			return R_Stanga(node);
+		}
+
+		// Cazul dreapta-stanga
+		if (balance(node) < -1 && balance(node->dreapta) > 0) {
+			node->dreapta = R_Dreapta(node->dreapta);
+			return R_Stanga(node);
+		}
+
+		return node;
+
+	}
+
+	void AfisInt(nod* node, int y, int z)
+	{
+		if (node != NULL)
+		{
+			AfisInt(node->stanga, y, z);
+			if(node->valoare <= z && node->valoare >= y)
+				fout << node->valoare << " ";
+			AfisInt(node->dreapta, y, z);
+		}
+	}
+
+	int find(nod* node, int valoare)
+	{
+		if (node == NULL)
+			return 0;
+
+		if (node->valoare == valoare)
+			return 1;
+		else if (node->valoare > valoare)
+			find(node->stanga, valoare);
+		else
+			find(node->dreapta, valoare);
+	}
+
+	//cel mai mic nr mai mare sau egal decat x
+	int min(nod* node, int x)
+	{	
+		int maxim = INT_MAX;
+		while (node != NULL) {
+			if (node->valoare == x) {
+				return x;
+			}
+			else if (node->valoare > x) {
+					maxim = node->valoare;
+					node = node->stanga;
+				}
+			else {
+				node = node->dreapta;
+			}
+		}
+		return maxim;
+	}
+
+	//cel mai mare nr mai mic sau egal decat x
+	int max(nod* node, int x)
+	{
+		int minim = INT_MIN;
+		while (node != NULL) {
+			if (node->valoare == x) {
+				return x;
+			}
+			else if (node->valoare < x) {
+				minim = node->valoare;
+				node = node->dreapta;
+			}
+			else {
+				node = node->stanga;
+			}
+		}
+		return minim;
 	}
 
 };
@@ -151,10 +314,58 @@ public:
 int main()
 {
 	AVL copacel;
-	nod* radacina = copacel.Insert(NULL, 10);
-	copacel.Insert(radacina, 20);
-	copacel.Insert(radacina, 5);
+	nod* radacina = NULL;
 
+	int n, x, y, z;
+	fin >> n;
+
+	for (int i = 0;i < n;++i)
+	{
+		fin >> x;
+
+		switch (x)
+		{
+		case(1):
+		{
+			fin >> y;
+			radacina = copacel.Insert(radacina, y);
+			break;
+		}
+		case(2):
+		{
+			fin >> y;
+			radacina = copacel.Delete(radacina, y);
+			break;
+		}
+		case(3):
+		{
+			fin >> y;
+			fout << copacel.find(radacina, y) << std::endl;
+			break;
+		}
+		case(4):
+		{
+			fin >> y;
+			fout << copacel.max(radacina, y) << std::endl;;
+			break;
+		}
+		case(5):
+		{
+			fin >> y;
+			fout << copacel.min(radacina, y) << std::endl;
+			break;
+		}
+		case(6):
+		{
+			fin >> y >> z;
+			copacel.AfisInt(radacina, y, z);
+			fout << std::endl;
+			break;
+		}
+		default:
+			break;
+		}
+	}
 
 	return 0;
 }
