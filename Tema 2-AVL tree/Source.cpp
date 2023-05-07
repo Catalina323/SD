7a862244c6abcf867d7036f566d54abca313e192
Catalina323 <116120497+Catalina323@users.noreply.github.com>
2023-05-07 11:07:57 +0300

    Update Source.cpp

--- Tema 2-AVL tree/Source.cpp
+++ Tema 2-AVL tree/Source.cpp
@@ -26,6 +26,66 @@ public:
 		}
 	}
 
+	nod* R_Stanga(nod* n)
+	{
+		nod* dr = n->dreapta;
+		nod* drst = dr->stanga;
+
+		dr->stanga = n;
+		n->dreapta = drst;
+
+		//refacem inaltimile
+		//pt nodul n
+		if (calch(n->dreapta) > calch(n->stanga))
+		{
+			n->inaltime = 1 + calch(n->dreapta);
+		}
+		else {
+			n->inaltime = 1 + calch(n->stanga);
+		}
+		//pt nodul dr
+		if (calch(dr->dreapta) > calch(dr->stanga))
+		{
+			dr->inaltime = 1 + calch(dr->dreapta);
+		}
+		else {
+			dr->inaltime = 1 + calch(dr->stanga);
+		}
+
+		return n;
+	}
+
+	nod* R_Dreapta(nod* n)
+	{
+		nod* st = n->stanga;
+		nod* stdr = st->dreapta;
+
+		st->dreapta = n;
+		n->stanga = stdr;
+
+		//refacem inaltimile
+		//pt nodul n
+		if (calch(n->dreapta) > calch(n->stanga))
+		{
+			n->inaltime = 1 + calch(n->dreapta);
+		}
+		else {
+			n->inaltime = 1 + calch(n->stanga);
+		}
+		//pt nodul st
+		if (calch(st->dreapta) > calch(st->stanga))
+		{
+			st->inaltime = 1 + calch(st->dreapta);
+		}
+		else {
+			st->inaltime = 1 + calch(st->stanga);
+		}
+
+		return n;
+	}
+
+
+
 	nod* Insert(nod* node, int valoare)
 	{
 		if (node == NULL)
@@ -59,30 +119,28 @@ public:
 		else 
 			balance = node->stanga->inaltime - node->dreapta->inaltime;
 
-		/*
+		
 		// Left Left Case
 		if (balance > 1 && valoare < node->stanga->valoare)
-			return rightRotate(node);
+			return R_Dreapta(node);
 
 		// Right Right Case
 		if (balance < -1 && valoare > node->dreapta->valoare)
-			return leftRotate(node);
+			return R_Stanga(node);
 
 		// Left Right Case
-		if (balance > 1 && valoare > node->stanga->key)
+		if (balance > 1 && valoare > node->stanga->valoare)
 		{
-			node->stanga = leftRotate(node->stanga);
-			return rightRotate(node);
+			node->stanga = R_Stanga(node->stanga);
+			return R_Dreapta(node);
 		}
 
 		// Right Left Case
 		if (balance < -1 && valoare < node->dreapta->valoare)
 		{
-			node->dreapta = rightRotate(node->dreapta);
-			return leftRotate(node);
+			node->dreapta = R_Dreapta(node->dreapta);
+			return R_Stanga(node);
 		}
-
-		*/
 
 		//return the (unchanged) node pointer 
 		return node;
